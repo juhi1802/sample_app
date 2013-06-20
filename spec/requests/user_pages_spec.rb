c9dc77ee741d819a632020124a5ad9d3bafc5f24
Juhi <bhatia.juhi786@gmail.com>
2013-06-20 13:39:41 +0530

    Test Failures sorted for unexpected end

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,17 +3,14 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
-
+  
   describe "signup page" do
       before { visit signup_path }
       
-      before { visit signup_path }
-
       let(:submit) { "Create my account" }
 
       it { should have_selector('h1',    text: 'Sign up') }
       it { should have_selector('title', text: 'Sign up') }
-    end
     
      describe "profile page" do
         let(:user) { FactoryGirl.create(:user) }
@@ -31,15 +28,20 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
+      
+        puts page.html
+        puts "*"*100
+        fill_in "Name",         with: "Example"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
 
       it "should create a user" do
+        puts page.html
+        puts "#"*100
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-
+  end
 end
