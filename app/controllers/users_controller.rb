b1392a221bce6a9a94f676c86e1ba35fe4bb48be
Juhi <bhatia.juhi786@gmail.com>
2013-06-21 14:53:20 +0530

    User Index-show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def new
     @user=User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
   end
   
   def create
@@ -19,7 +26,7 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
   
   def update
@@ -31,5 +38,17 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
