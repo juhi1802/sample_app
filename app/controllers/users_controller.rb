250f88ee0f4870965a0d9188aa1cd34abcc50712
Juhi <bhatia.juhi786@gmail.com>
2013-06-24 12:08:44 +0530

    Add, Delete, Show user posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def index
@@ -47,12 +48,12 @@ class UsersController < ApplicationController
     end
   end
   
-  def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+  # def signed_in_user
+      # unless signed_in?
+        # store_location
+        # redirect_to signin_url, notice: "Please sign in."
+      # end
+    # end
   
   def correct_user
     @user = User.find(params[:id])
--- spec/factories.rb
+++ spec/factories.rb
@@ -18,5 +18,11 @@ FactoryGirl.define do
       admin true
     end
   end
+  
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+  
 end
 
